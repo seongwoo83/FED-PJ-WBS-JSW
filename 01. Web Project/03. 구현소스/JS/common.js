508241fb1eb0708ff1561c1dd9ce4a1cd5a06dda
seongwoo83 <jeongseongwoo83@daum.net>
2023-02-06 17:20:11 +0900

    02/06 17:20

--- 01. Web Project/03. 구현소스/JS/common.js
+++ 01. Web Project/03. 구현소스/JS/common.js
@@ -7,9 +7,11 @@ window.addEventListener("load", function(){
     for(let i = 0; i<gnbList.length; i++){
         gnbList[i].onmouseover = function(){
             navList[i].style.display = "block";
+            // navList[i].style.height = "100%";
         }
         gnbList[i].onmouseleave = function(){
             navList[i].style.display = "none";
+            // navList[i].style.height = "0";
         }
     }
 })
