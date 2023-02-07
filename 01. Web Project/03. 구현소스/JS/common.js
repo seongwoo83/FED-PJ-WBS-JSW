93f8522f422207615c208d1c69dce7ec317d3319
seongwoo83 <jeongseongwoo83@daum.net>
2023-02-07 17:19:23 +0900

    02/07 17:19

--- 01. Web Project/03. 구현소스/JS/common.js
+++ 01. Web Project/03. 구현소스/JS/common.js
@@ -1,17 +1,16 @@
 window.addEventListener("load", function(){
     let gnbList = document.querySelectorAll(".gnb>ul>li");
     let navList = document.querySelectorAll(".gnb>ul>li .smenu_wrap");
-    console.log('gnbList: ', gnbList);
-    console.log('navList: ', navList);
+    // console.log('gnbList: ', gnbList);
+    // console.log('navList: ', navList);
 
     for(let i = 0; i<gnbList.length; i++){
         gnbList[i].onmouseover = function(){
-            navList[i].style.display = "block";
-            // navList[i].style.height = "100%";
+            navList[i].style.height = this.querySelector(".smenu").clientHeight+"px"
         }
         gnbList[i].onmouseleave = function(){
-            navList[i].style.display = "none";
-            // navList[i].style.height = "0";
+            navList[i].style.height = "0";
+            navList[i].style.overflow = "hidden";
         }
     }
 })
