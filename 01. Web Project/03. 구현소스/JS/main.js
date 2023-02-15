fd6726a465d77a8e24da2be29477a0155b96d947
seongwoo83 <jeongseongwoo83@daum.net>
2023-02-15 17:19:42 +0900

    02/15 17:19

--- 01. Web Project/03. 구현소스/JS/main.js
+++ 01. Web Project/03. 구현소스/JS/main.js
@@ -1,5 +1,22 @@
 window.addEventListener("DOMContentLoaded",()=>{
-    let mvisual = document.querySelectorAll(".event");
+    let promo = document.querySelector(".right_promo button");
+    let promoBtn = document.querySelector(".promo_btn img");
+    let promoMenu = document.querySelector(".menu_promo");
+    promo.onclick = ()=>{
+        let arrowTxt = "";
+        arrowTxt = promoBtn.getAttribute("src");
+        if(arrowTxt === "./03. 구현소스/img/starbucks/main_promo_down.png"){
+            arrowTxt = "./03. 구현소스/img/starbucks/main_promo_up.png"
+            promoBtn.setAttribute("src", arrowTxt);
+            promoMenu.style.display = "block";
+        }else{
+            arrowTxt = "./03. 구현소스/img/starbucks/main_promo_down.png"
+            promoBtn.setAttribute("src", arrowTxt);
+            promoMenu.style.display = "none";
+        }
+    }
     
 
-})
+
+
+});
