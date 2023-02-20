f79d6fa4ab219c2a1c071729c400595e5de7c484
seongwoo83 <jeongseongwoo83@daum.net>
2023-02-20 17:20:01 +0900

    02/20 17:20

--- 01. Web Project/03. 구현소스/JS/main.js
+++ 01. Web Project/03. 구현소스/JS/main.js
@@ -2,6 +2,12 @@ window.addEventListener("DOMContentLoaded",()=>{
     let promo = document.querySelector(".right_promo button");
     let promoBtn = document.querySelector(".promo_btn img");
     let promoMenu = document.querySelector(".menu_promo");
+    let promoSlider = document.querySelector(".promo_slider");
+    let promoBx = document.querySelectorAll(".promo_box");
+    let btns = document.querySelectorAll(".menu_promo button");
+
+    
+    
     promo.onclick = ()=>{
         let arrowTxt = "";
         arrowTxt = promoBtn.getAttribute("src");
@@ -16,37 +22,37 @@ window.addEventListener("DOMContentLoaded",()=>{
         }
     }
     
-    let promoSlider = document.querySelector(".promo_slider");
-    let promoBx = document.querySelectorAll(".promo_box");
-    let btns = document.querySelectorAll(".promo_wrap button");
     btns.forEach((ele,idx)=>{ // ele => 요소자신, idx => 요소 순번
         ele.onclick = ()=>goSlide(idx);
     });
+
     let promoSeq = 1;
+    promoSlider.style.transform = `translateX(-819px)`;
 
     const goSlide = (seq)=>{
         /* 1. 함수 호출 및 전달값 확인 */
         console.log("호출완료",seq);
-;
-        /* 현재전역 inum변수 */
-        console.log("현재전역 promoSeq: ",promoSeq);
+        
+        /* 현재전역 promoSeq변수 */
         /* 2. 버튼 별 분기하기 */
-        /* 오른쪽 버튼 클릭*/
         if(seq){
             promoSeq++;//1씩증가
-            /* 한계값체크 */
-            if(promoSeq===4) promoSeq = 1;
-        }
-        /* 왼쪽 버튼 클릭 */
-        else{
+        }else{
             promoSeq--;//1씩감소
-            /* 한계값체크 */
-            if(promoSeq === 0) promoSeq = 3;
         }
-        /* 증감후 promoSeq변수 */
-        console.log("증감후 promoSeq: ",promoSeq);
-        let slidePx = 819*promoSeq;
-        promoSlider.style.transform = `translateX(-${slidePx}px)`;
+        console.log("증감후",promoSeq);
+        if(promoSeq===4){
+            promoSeq = 1;    
+        }else if(promoSeq === -1){
+            promoSeq = 2;
+        }
+        else if(promoSeq === 0) {
+            promoSeq = 3;
+        }
+        console.log("초기화후",promoSeq);
+        let slidePx = -(819*promoSeq);
+        promoSlider.style.transform = `translateX(${slidePx}px)`;
+        promoSlider.style.transition = "transform .4s ease-in-out"; 
         
     };
 
