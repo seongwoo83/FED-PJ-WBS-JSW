47c05c51fd00d88596bd66480ab057f301a44bda
seongwoo83 <jeongseongwoo83@daum.net>
2023-04-24 12:52:39 +0900

    04/24 12:52

--- 02.Mobile_Web&App_Project/03.sources/js/common.js
+++ 02.Mobile_Web&App_Project/03.sources/js/common.js
@@ -3,8 +3,13 @@ $(()=>{
     $(document).on("mouseover", ".category", function(){
         console.log("터치");
         console.log($(this).find(".smenu").height())
+        $(this).find("div").animate({
+            height: $(".smenu", this).height()+"px"
+        },400);
     })
     $(document).on("mouseout", ".category", function(){
-        $(this).find("div").hide(400);
+        $(this).find("div").animate({
+            height: "0"
+        },400);
     })
 })
