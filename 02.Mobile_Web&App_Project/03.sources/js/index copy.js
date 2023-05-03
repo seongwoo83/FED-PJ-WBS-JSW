f5e3565aa0761dc17568a3aba493852a7fbd13d7
seongwoo83 <jeongseongwoo83@daum.net>
2023-05-03 13:20:15 +0900

    05/03

--- 02.Mobile_Web&App_Project/03.sources/js/index copy.js
+++ 02.Mobile_Web&App_Project/03.sources/js/index copy.js
@@ -1,5 +1,18 @@
-window.addEventListener("DOMContentLoaded",function(){
-
+window.addEventListener("DOMContentLoaded", function () {
+    let scTop = 0;
+    $(window).on("scroll",function(){
+        scTop = $(this).scrollTop();
+        console.log(scTop);
+        if(scTop>100){
+            $(".top .inner").animate({
+                backgroundColor: "#fff"
+            })
+        }else{
+            $(".top .inner").animate({
+                backgroundColor: "transparent"
+            })
+        }
+    })
     const tpg = document.querySelector(".vt_wrap");
     const spg = document.querySelector(".hr_wrap");
 
@@ -20,18 +33,39 @@ window.addEventListener("DOMContentLoaded",function(){
         }
     }
     console.log("로딩됨");
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
     var swiper3 = new Swiper(".mySwiper3", {
         grabCursor: true,
         effect: "creative",
         loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
         creativeEffect: {
-        prev: {
-            shadow: true,
-            translate: ["0%", 0, -1],
+            prev: {
+                shadow: true,
+                translate: ["0%", 0, -1],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
         },
-        next: {
-            translate: ["100%", 0, 0],
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                progressCircle.style.setProperty("--progress", 1 - progress);
+                progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+            },
         },
     });
-})
+});
