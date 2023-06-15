5d30c56b1ed7ad22e922aac61d3093fbf3d8957e
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-15 11:20:27 +0900

    06/15

--- 03.SPA_Project/03.sources/spa_project/src/ferrari/module/Model.js
+++ 03.SPA_Project/03.sources/spa_project/src/ferrari/module/Model.js
@@ -1,64 +1,140 @@
-import React from 'react';
-import $ from 'jquery';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation} from 'swiper';
-import {SlideNextButton, SlidePrevButton} from './Swiperbutton';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import '../css/model.css';
-import mdata from '../data/mdata';
+import React from "react";
+import $ from "jquery";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper";
+import { SlideNextButton, SlidePrevButton } from "./Swiperbutton";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "../css/model.css";
+import mdata from "../data/mdata";
 
-
-function Model(){
-    return(
-        <div className='swiper_wrap'>
-        <Swiper
-            modules={[ Navigation]}
-            spaceBetween={10}
-            slidesPerView={1.2}
-            loop={true}
-            scrollbar={{draggable: true}}
-            onSwiper={()=>$('.swiper-slide-active').find(".tit_wrap").css({
-                opacity:1
-            }).parents(".swiper-slide").css({
-                backgroundColor:"#888"
-            })}
-            onSlideChangeTransitionStart={()=>$(".swiper-slide-active").find(".tit_wrap").animate({
-                opacity:1
-            }).parent().siblings(".swiper-slide").find(".tit_wrap").animate({
-                opacity:0
-            })}
-            breakpoints={{
-                700:{
-                    slidesPerView:2,
-                    spaceBetween:-50
-                },
-                1200:{
-                    slidesPerView:3,
-                    spaceBetween:-300
+function Model() {
+    return (
+        <div className="swiper_wrap">
+            <Swiper
+                modules={[Navigation, Autoplay]}
+                spaceBetween={10}
+                slidesPerView={1.2}
+                loop={true}
+                speed={500}
+                allowTouchMove={false}
+                autoplay={{
+                    delay:2000
+                }}
+                onSwiper={() =>
+                    $(".swiper-slide-active")
+                        .find(".tit_wrap")
+                        .css({
+                            opacity: 1,
+                        })
+                        .parents(".swiper-slide-active")
+                        .css({
+                            zIndex: 3,
+                        })
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "transparent",
+                        })
+                        .parent()
+                        .next()
+                        .css({
+                            zIndex: 2,
+                        })
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "rgba(0,0,0,0.2)",
+                        })
+                        .parent()
+                        .next()
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "rgba(0,0,0,0.5)",
+                        })
+                        .parent()
+                        .css({
+                            zIndex: 1,
+                        })
                 }
-            }}
-        >
-            {
-                mdata.map((v, i)=>{
+                onSlideChangeTransitionStart={() =>
+                    $(".swiper-slide-active")
+                        .find(".tit_wrap")
+                        .animate({
+                            opacity: 1,
+                        })
+                        .parent()
+                        .prev()
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "rgba(0,0,0,0.8)",
+                        })
+                        .parent()
+                        .css({
+                            zIndex: 0,
+                            backgroundColor: "transparent",
+                        }).delay(200)
+                        .next()
+                        .css({
+                            zIndex: 3,
+                            backgroundColor: "#fff",
+                        })
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "transparent",
+                        })
+                        .parent()
+                        .next()
+                        .css({
+                            zIndex: 2,
+                        })
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "rgba(0,0,0,0.2)",
+                        })
+                        .parent()
+                        .css({
+                            zIndex: 1,
+                        })
+                        .find(".cover")
+                        .css({
+                            backgroundColor: "rgba(0,0,0,0.5)",
+                        })
+                        .parents(".swiper-slide-active")
+                        .siblings(".swiper-slide")
+                        .find(".tit_wrap")
+                        .animate({
+                            opacity: 0,
+                        })
+                }
+                breakpoints={{
+                    700: {
+                        slidesPerView: 2,
+                        spaceBetween: -40,
+                    },
+                    1200: {
+                        slidesPerView: 2,
+                        spaceBetween: -350,
+                    },
+                }}
+            >
+                {mdata.map((v, i) => {
                     return (
                         <SwiperSlide key={i}>
-                            <div className='img_wrap' >
-                                <img src={v.src} alt='차'  />
+                            <div className="cover"></div>
+                            <div className="img_wrap">
+                                <img src={v.src} alt="차" />
                             </div>
-                            <div className='tit_wrap'>
-                                <span className='tit'>{v.name.toUpperCase()}</span>
+                            <div className="tit_wrap">
+                                <span className="tit">{v.name.toUpperCase()}</span>
                             </div>
                         </SwiperSlide>
                     );
-                })
-            }
-            <div className='btn_wrap'>
-                <SlidePrevButton />
-                <SlideNextButton />
-            </div>
-        </Swiper>
+                })}
+                <div className="btn_wrap">
+                    <SlidePrevButton />
+                    <SlideNextButton />
+                </div>
+            </Swiper>
         </div>
     );
 }
