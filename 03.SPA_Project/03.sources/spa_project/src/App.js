ad29ededd52a89df56c3a9ae1616b67a0051ebbc
seongwoo83 <jeongseongwoo83@daum.net>
2023-06-16 16:04:38 +0900

    06/16

--- 03.SPA_Project/03.sources/spa_project/src/App.js
+++ 03.SPA_Project/03.sources/spa_project/src/App.js
@@ -5,11 +5,13 @@ import Info from './ferrari/module/Info';
 import News from './ferrari/module/News';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Test from './ferrari/module/Test';
+import Ban from './ferrari/module/Ban';
 
 function App() {
   return (
     <>
       <Test />
+      <Ban />
       <Model />
       <News />
       <Board />
--- 03.SPA_Project/03.sources/spa_project/src/ferrari/module/Ban.js
+++ 03.SPA_Project/03.sources/spa_project/src/ferrari/module/Ban.js
@@ -1,11 +1,56 @@
-import Swiper from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import '../css/ban.css';
 function Ban(){
+
+    const slide_data = [
+        {
+            stit:"Cars",
+            btit:"Start Your Engine",
+            src:"https://ferrari-cdn.thron.com/static/Z2DSEE_SITO-Video_Cover_Gen_2023_768x1024_J6QFKV.mp4?xseo=",
+        },
+        {
+            stit:"",
+            btit:"SCUDERIA FERRARI",
+            src:"https://ferrari-cdn.thron.com/static/M9YPMI_cover_f1-sf23_video_beauty_768x1024__720p_VTPLDP.mp4?xseo=",
+        },
+        {
+            stit:"Collections",
+            btit:"NEW 2023 SPRING-SUMMER COLLECTION",
+            src:"https://ferrari-cdn.thron.com/static/HVQBCA_Store_Ferrari_Cover_768x1024_CP6ON5.mp4?xseo=",
+        }
+    ]
+
+
+
     return(
-        <>
-        <Swiper>
-            
+        <div>
+        <Swiper
+        modules={[Autoplay]}
+        loop={true}
+        speed={500}
+        autoplay={{
+            delay:7000
+        }}>
+            {
+                slide_data.map((v,i)=>{
+                    return(
+                        <SwiperSlide key={i} className="video_slide">
+                            <div className="v_cont_bx">
+                                {
+                                    v.stit !== null &&
+                                    <h4 className="v_stit">{v.stit}</h4>
+                                }
+                                <h2 className="v_btit">{v.btit}</h2>
+                                <button className="v_btn">Show Details</button>
+                            </div>
+                            <video src={v.src} muted="muted" autoPlay="autoplay" loop="loop" className="v_src"/>
+                        </SwiperSlide>
+                    )
+                })
+            }
         </Swiper>
-        </>
+        </div>
     );
 }
 
